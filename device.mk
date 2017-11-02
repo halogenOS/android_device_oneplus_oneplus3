73aff5a81d2e8b0621b2e59370bef68aa6428986
Simao Gomes Viana <xdevs23@outlook.com>
2017-11-02 22:09:42 +0100

    use sensors.default
    Change-Id: I1e2ef1cd64fe18d4b88a9fe76adcf9201e90fe86

--- device.mk
+++ device.mk
@@ -340,7 +340,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    sensors.default
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
