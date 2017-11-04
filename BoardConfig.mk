24bb69cb8fa052ef03b978e68f8c572f10095773
Harsh Shandilya <msfjarvis@gmail.com>
2017-11-04 17:12:30 +0530

    oneplus3: Switch to AOSP HALs
    * Testing in production ftw
    
    Change-Id: Id2d6ac722fcbf5ca251753e86988beb2ba2c59e4
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,7 @@ TARGET_KERNEL_CONFIG := oneplus3_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
+# BOARD_USES_QCOM_HARDWARE := true
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-uart"
