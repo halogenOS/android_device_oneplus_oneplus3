7245fb02e3266c48eeb7326988458d21da9abbc7
dianlujitao <dianlujitao@lineageos.org>
2017-11-04 14:07:54 +0800

    op3: Remove ipacm/ipanat
     * Disabled by Qualcomm in O
    
    Change-Id: I063355c2c0bf188bcbafca09f2ea5569a0e5d2f2
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- device.mk
+++ device.mk
@@ -391,9 +391,8 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    ipacm \
-    ipacm-diag \
-    IPACM_cfg.xml \
+    libqsap_sdk \
+    libQWiFiSoftApCfg \
     libwpa_client \
     wificond \
     hostapd \
